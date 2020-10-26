cc716faed7308bbd183900678a5b7907e6e2ab63
n_dosya <adosiak@impressit.io>
2020-10-26 15:04:14 +0200

    added custome checkbox

--- src/pages/Authorization/Authorization.tsx
+++ src/pages/Authorization/Authorization.tsx
@@ -15,48 +15,45 @@ const Authorization: React.FC = () => {
             </span>
             <Formik
                 initialValues={{email: '', password: '', checked: []}}
-                onSubmit={(event) => console.log(event)}
-            >
+                onSubmit={(event) => console.log(event)}>
                 {({}) => (
                     <Form className="login__form">
-                        <label className="login__form-field-label">
+                        <label className="field__label">
                             Email
                         </label>
-                        <Field type="email" name="email" className="login__form-field"  placeholder="email@email.com"/>
-                        <label className="login__form-field-label">
+                        <Field type="email" name="email" className="field" placeholder="email@email.com"/>
+                        <label className="field__label">
                             Password
                         </label>
-                        <Field type="password" name="password" className="login__form-field" placeholder="Password"/>
-                        <label className="login__form-label">
-                            <Field type="checkbox" name="checked" value="Agree" className="login__form-checkbox"/>
-                            I read and agree with	&nbsp;
-                            <a className="login__form-label-link">
-                                privacy policy	&nbsp;
+                        <Field type="password" name="password" className="field" placeholder="Password"/>
+                        <label className="checkbox__label">
+                            <Field type="checkbox" name="checked" value="Agree" className="login__form-input"/>
+                            <span className="checkbox__checkmark"></span>
+                            I read and agree with&nbsp;
+                            <a className="login__form-link">
+                                privacy policy&nbsp;
                             </a>
-                            and	&nbsp;
+                            and&nbsp;
                             <a className="login__form-label-link">
                                 terms of service
                             </a>
                         </label>
                         <div className="login__form-buttons-container">
-                        <GoogleLogin
-                            clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-                            buttonText="Sign in with google"
-                            onSuccess={responseGoogle}
-                            onFailure={responseGoogle}
-                            className="login__form-button-google"
-                            cookiePolicy={'single_host_origin'}
-                        />
-                        <button type="submit" className="login__form-button-submit">
-                            Log in
-                        </button>
+                            <GoogleLogin
+                                clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+                                buttonText="Sign in with google"
+                                onSuccess={responseGoogle}
+                                onFailure={responseGoogle}
+                                className="login__form-button-google"
+                                cookiePolicy={'single_host_origin'}
+                            />
+                            <button type="submit" className="login__form-button-submit">
+                                Log in
+                            </button>
                         </div>
                     </Form>
                 )}
             </Formik>
-            <div>
-
-            </div>
         </div>
     );
 };
